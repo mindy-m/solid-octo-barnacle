94c1d42a7fd2e9c48641bef648260c0ce4f9c9e8
Mindy Medeiros <mmedei6@wgu.edu>
2023-11-10 02:12:05 -0500

    Put statements on their own line.

--- for_statement_practice.c
+++ for_statement_practice.c
@@ -5,5 +5,5 @@ main()
     int goat_count;
 
     for (goat_count = 0; goat_count <= 25; goat_count++)
-        printf("I have %d goats!", goat_count);
+        printf("I have %d goats!\n", goat_count);
 }
